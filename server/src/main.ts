13eaa2d92cb45ce4c2467d2af23761bb144db82b
RuslanManoilo <110188082+RuslanManoilo@users.noreply.github.com>
2024-05-19 23:56:31 +0300

    created server + local db

---
+++ server/src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+async function bootstrap() {
+  const PORT = process.env.PORT || 5000;
+
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Website Tracker")
+    .setDescription("Документація REST API")
+    .setVersion("1.0.0")
+    // .addTag("Users")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("/api/docs", app, document);
+
+  await app.listen(PORT, () =>
+    console.log(`Server succesfully running on ${PORT} Port`)
+  );
+}
+
+bootstrap();
