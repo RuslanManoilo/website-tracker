a1c15804532e49dca21a80a1e0d9ebd070e86806
RuslanManoilo <110188082+RuslanManoilo@users.noreply.github.com>
2024-07-20 14:20:32 +0300

    Ovral initialised + Api's generated

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "http://localhost:8008/:path*",
+      }
+    ]
+  }
 };
 
 export default nextConfig;
